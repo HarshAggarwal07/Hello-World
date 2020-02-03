f820ab84b52b7db011e9aecc76e51d43504c9c45
HarshAggarwal07 <38116092+HarshAggarwal07@users.noreply.github.com>
2020-02-03 12:20:28 +0530

    Add files via upload

---
+++ Trainee.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TraineeAssessmentApp
+{
+    class Trainee
+    {
+        public String Name { get; set; }
+        public int assignment { get; set; }
+        public int[] score { get; set; }
+    }
+}
